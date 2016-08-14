bfb89b748a5c85ef073bc7befaab626d2eb14515
Mudit <Mudit@DESKTOP-GI0M7V2>
2016-08-13 22:08:34 -0400

    Added Models
    Added Invoice Model
    Added Transaction Model

---
+++ src/main/java/org/mudit/Simplyvyapar/model/Invoice.java
@@ -0,0 +1,62 @@
+package org.mudit.Simplyvyapar.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+public class Invoice {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	@ManyToOne
+	private Product product;
+	private float amount;
+	private boolean status;
+	private Transaction transaction;
+	private Date date;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public float getAmount() {
+		return amount;
+	}
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public Transaction getTransaction() {
+		return transaction;
+	}
+	public void setTransaction(Transaction transaction) {
+		this.transaction = transaction;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
---
+++ src/main/java/org/mudit/Simplyvyapar/model/Transaction.java
@@ -0,0 +1,5 @@
+package org.mudit.Simplyvyapar.model;
+
+public class Transaction {
+
+}
