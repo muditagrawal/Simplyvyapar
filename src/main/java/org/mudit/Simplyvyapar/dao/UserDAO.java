5d4a5a427c959a36589ec9cc3d6dd6275c5d4987
Mudit <Mudit@DESKTOP-GI0M7V2>
2016-08-15 13:16:46 -0400

    Created DAO package
    Created UserDAO skeleton

---
+++ src/main/java/org/mudit/Simplyvyapar/dao/UserDAO.java
@@ -0,0 +1,19 @@
+package org.mudit.Simplyvyapar.dao;
+
+public class UserDAO {
+	public void createUser() {
+		
+	}
+	public void updateUser() {
+		
+	}
+	public void deleteUser() {
+		
+	}
+	public void getUserById() {
+		
+	}
+	public void getUserByName() {
+		
+	}
+}
